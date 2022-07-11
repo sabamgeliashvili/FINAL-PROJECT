b96c48cc7b27ad19d64257fa7f1965a02b2033f8
sabamgeliashvili <sba69717@gmail.com>
2022-07-11 16:26:20 +0400

    .

--- script.js
+++ script.js
@@ -8,3 +8,17 @@
         }
     });
 });
+
+//    burger-menu
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+if(!menuOpen){
+    menuBtn.classList.add('open');
+    menuOpen = true;
+} else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+}
+});
