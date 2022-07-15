43f30e812f81ec8f85784492d682c4f2a11b50c4
sabamgeliashvili <sba69717@gmail.com>
2022-07-16 00:16:50 +0400

    add meta tags

--- script.js
+++ script.js
@@ -1,37 +1,14 @@
-// (function(){
-//     $(window).on("scroll", function() {
-//         if($(window).scrollTop() > 50) {
-//             $(".header").addClass("active");
-//         } else {
-//             //remove the background property so it comes transparent again (defined in your css)
-//            $(".header").removeClass("active");
-//         }
-//     });
-// });
 
-//    burger-menu
+// burger menu 
 
-// const menuBtn = document.querySelector('.menu-btn');
-// let menuOpen = false;
-// menuBtn.addEventListener('click', () => {
-// if(!menuOpen){
-//     menuBtn.classList.add('open');
-//     menuOpen = true;
-// } else {
-//     menuBtn.classList.remove('open');
-//     menuOpen = false;
-// }
-// });
-
-
-
-
-// burger menu 2
  const hamburger = document.querySelector('.hamburger');
  const navmenu = document.querySelector('.nav-menu');
  hamburger.addEventListener('click', () => {
     hamburger.classList.toggle ("active");
     navmenu.classList.toggle ("active");
  })
+
+
+ document.getElementById('submitbtn').addEventListener('click', function() { alert('your message was sent successfully') })
 
 
